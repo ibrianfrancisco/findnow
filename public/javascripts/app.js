dfe139e76888f2814680f97c6fd2aef258e68792
Brian Francisco <ibrianfrancisco@gmail.com>
2017-07-05 23:30:42 -0700

    styled welcome page

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,7 +1,7 @@
 (function() {
 'use strict';
 
-  angular.module('app', ['ui.router', 'ngAnimate', 'ngResource'])
+  angular.module('app', ['ui.router', 'ngResource'])
     .config(configRoutes);
 
   configRoutes.$inject = ['$stateProvider', '$urlRouterProvider', '$httpProvider'];
