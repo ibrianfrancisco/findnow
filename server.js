08bc06707a80de1060b31fd82f8a3a0000eaaf53
Brian Francisco <ibrianfrancisco@gmail.com>
2017-07-05 17:47:32 -0700

    took out method-override

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
+// var methodOverride = require('method-override');
 require('dotenv').config();
 require('./config/database');
 
