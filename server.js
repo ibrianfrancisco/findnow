5d00ad2e1aeae39fb975a98c1dfb3a76bd1355ec
Brian Francisco <ibrianfrancisco@gmail.com>
2017-07-05 17:50:28 -0700

    took out method override

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(methodOverride('_method'))
+// app.use(methodOverride('_method'))
 app.use('/', index);
 app.use('/api', apiRoutes);
 
