9cbfb587450fcd542a68c1d2f00e82b253140964
Brian Francisco <ibrianfrancisco@gmail.com>
2017-07-11 16:08:37 -0700

    fixed banner width

--- gulpfile.js
+++ gulpfile.js
@@ -6,16 +6,20 @@ const gulp = require('gulp'),
       autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('minify-css', () => {
-  return gulp.src('public/stylesheets/*.css')
+  return gulp.src('public/stylesheets/style.css')
     .pipe(autoprefixer())
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('public/dist/css'));
 });
 
 gulp.task('browserify', () => {
-  return gulp.src(['./public/javascripts/app.js',
-                   './public/javascripts/services/*.js'])
+  return gulp.src(['public/javascripts/app.js',
+                   'public/javascripts/services/*.js'])
   .pipe(buffer())
   .pipe(uglify())
-  .pipe(gulp.dest('./public/dist/js'));
+  .pipe(gulp.dest('public/dist/js'));
 });
+
+gulp.task('default', ['browserify', 'minify-css']);
+
+gulp.watch(['public/javascripts/**/*.js', 'public/stylesheets/*.css'], ['default']);
